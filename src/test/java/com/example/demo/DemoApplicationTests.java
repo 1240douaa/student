4b75a507bf873bfbd728a6119918f157f5a37d90
doua <douah503@gmail.com>
2025-11-25 22:18:17 +0100

    Add DemoApplication to satisfy cached test requirements

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Test basique qui vérifie que le contexte Spring se charge
+	}
+
+}
